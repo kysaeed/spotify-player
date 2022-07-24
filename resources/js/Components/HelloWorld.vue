f06d0886212ed2e8235ce84f89aa907cdb1f483a
kysaeed <kysaeed@gmail.com>
2022-07-24 10:53:05 +0900

    vue + vite

---
+++ resources/js/Components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="hello-box">
+        <h1>やあ、世界！</h1>
+        <div>hello world</div>
+        <h2>カウンタ：{{ a }}</h2>
+        <div>
+            <button @click="add">add!</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'hello-world',
+
+    methods: {
+        add() {
+            this.a++
+        },
+    },
+    data() {
+        return {
+            a: 0,
+        }
+    },
+}
+</script>
+
+<style land="scss" scoped>
+.hello-box {
+    position: relative;
+    margin: 20px;
+    padding: 10px;
+    border: 1px solid #101010;
+    border-radius: 10px;
+}
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import HelloWorld from './Components/HelloWorld.vue'
+
+createApp(HelloWorld).mount('#app')
